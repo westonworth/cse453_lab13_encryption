e4dc7190ad87824afba95caea516b96f01781187
westonworth <65480669+westonworth@users.noreply.github.com>
2021-07-17 21:46:35 -0700

    Update cipher03.h

--- cipher03.h
+++ cipher03.h
@@ -12,10 +12,10 @@
 class Cipher03 : public Cipher
 {
 public:
-   virtual std::string getPseudoAuth()  { return "pseudocode author"; }
-   virtual std::string getCipherName()  { return "cipher name"; }
-   virtual std::string getEncryptAuth() { return "encrypt author"; }
-   virtual std::string getDecryptAuth() { return "decrypt author"; }
+   virtual std::string getPseudoAuth()  { return "Weston Gavin"; }
+   virtual std::string getCipherName()  { return "Columnar Transposition"; }
+   virtual std::string getEncryptAuth() { return "Weston Gavin"; }
+   virtual std::string getDecryptAuth() { return "Weston Gavin"; }
 
    /***********************************************************
     * GET CIPHER CITATION
@@ -23,7 +23,7 @@ public:
     ***********************************************************/
    virtual std::string getCipherCitation()
    {
-      return std::string("citation");
+      return std::string("Kahn, David. The Codebreakers: The Story of Secret Writing. Rev Sub. Scribner, 1996.");
    }
    
    /**********************************************************
@@ -34,12 +34,20 @@ public:
    {
       std::string str;
 
-      // TODO: please format your pseudocode
-      // The encrypt pseudocode
-      str =  "insert the encryption pseudocode\n";
-
+      
+      str =  "Columnar Transposition\n";
+      str += "GET password LENGTH\n";
+      str += "CREATE multi-dimensional array\n";
+      str += "SET Row Lengths = password LENGTH\n";
+      str += "SET counter = 0\n";
+      str += "COUNT rows & leftover elements on last row\m;
+      str += "WRITE until rows and columns exceeded\n";
       // The decrypt pseudocode
-      str += "insert the decryption pseudocode\n";
+      str += "Decryption Pseudocode\n";
+      str += "GET password LENGTH\n";
+      str += "SET Row Lengths = password LENGTH\n";
+      str += "SET counter = 0\n";
+      str += "READ letters at row length offsets until rows and columns exceeded\n;
 
       return str;
    }
@@ -51,8 +59,35 @@ public:
    virtual std::string encrypt(const std::string & plainText,
                                const std::string & password)
    {
-      std::string cipherText = plainText;
-      // TODO - Add your code here
+      int numCols = password.length();
+      int colModulusCountdown = plainText.length() % numCols;
+      int rowCounter = (plainText.length() / password.length()) + 1;
+
+      char array[100][100];
+      int counter = 0;
+      for (int j = 0; j < rowCounter; j++)
+      {
+         for (int i = 0; i < password.length(); i++)
+            {
+               if ((j != rowCounter) || (i < colModulusCountdown))
+                  {
+                     array[j][i] = plainText[counter];
+                     counter++;
+                  }
+            }
+    
+      }
+      string cipherText;
+      for (int k = 0; k < password.length(); k++)
+         {
+            for (int l = 0; j < rowCounter; l++)
+               {
+                  if ((l != rowCounter) || (k < colModulusCountdown))
+               {
+                ciphertext += array[l][k];
+         }
+    }   
+}
       return cipherText;
    }
 
@@ -63,8 +98,19 @@ public:
    virtual std::string decrypt(const std::string & cipherText,
                                const std::string & password)
    {
-      std::string plainText = cipherText;
-      // TODO - Add your code here
+      int numCols = password.length();
+      int colModulusCountdown = cipherText.length() % numCols;
+      int rowCounter = (cipherText.length() / password.length()) + 1;
+      
+      for (int colLetter = 0; colLetter < password.length(); colLetter++)
+      {
+         int newCount = 0;
+         while (newCount < cipherText.length())
+         {
+            std::cout << cipherText[newCount];
+            newCount += password.length();
+         }
+       }
       return plainText;
    }
 };
